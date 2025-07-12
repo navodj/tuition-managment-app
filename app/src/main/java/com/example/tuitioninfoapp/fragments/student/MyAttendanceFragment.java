e6ff723c8409cb8e6a352f3da9a8900b5400fd0d
Navod <jayawardananavodlk@gmail.com>
2025-07-12 14:03:58 +0530

    added student attendance qr code

--- app/src/main/java/com/example/tuitioninfoapp/fragments/student/MyAttendanceFragment.java
+++ app/src/main/java/com/example/tuitioninfoapp/fragments/student/MyAttendanceFragment.java
@@ -1,66 +1,57 @@
 package com.example.tuitioninfoapp.fragments.student;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
-
+import android.view.*;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.example.tuitioninfoapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.zxing.BarcodeFormat;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MyAttendanceFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MyAttendanceFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private ImageView qrImageView;
+    private TextView titleText;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public MyAttendanceFragment() {}
 
-    public MyAttendanceFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyAttendanceFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MyAttendanceFragment newInstance(String param1, String param2) {
-        MyAttendanceFragment fragment = new MyAttendanceFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_q_r_code_generator, container, false);
+
+        qrImageView = view.findViewById(R.id.qr_image);
+        titleText = view.findViewById(R.id.text_title);
+
+        generateQRCode();
+
+        return view;
+    }
+
+    private void generateQRCode() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String uid = user.getUid();
+            String name = user.getDisplayName() != null ? user.getDisplayName() : "Student";
+            String content = uid; // or use JSON: "{\"uid\":\"" + uid + "\"}"
+
+            try {
+                BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                Bitmap bitmap = barcodeEncoder.encodeBitmap(content,
+                        BarcodeFormat.QR_CODE, 500, 500);
+                qrImageView.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_attendance, container, false);
     }
 }
